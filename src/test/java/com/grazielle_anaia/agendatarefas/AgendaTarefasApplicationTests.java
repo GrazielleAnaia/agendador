1b30b656f80431c3b6b646850e50e6358f629648
Grazielle <grazielle.anaia@gmail.com>
2025-02-06 09:43:25 -0500

    Commit initial

---
+++ src/test/java/com/grazielle_anaia/agendatarefas/AgendaTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grazielle_anaia.agendatarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
