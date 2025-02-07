f31f03fc3120bb2f2338a6a6f369585cbaaf1585
Grazielle <grazielle.anaia@gmail.com>
2025-02-07 12:00:26 -0500

    Added methods Get by email and by time frame

--- src/main/java/com/grazielle_anaia/agendatarefas/business/dto/TarefasDTO.java
+++ src/main/java/com/grazielle_anaia/agendatarefas/business/dto/TarefasDTO.java
@@ -22,12 +22,15 @@ public class TarefasDTO {
 
     private String descricao;
 
+    @JsonFormat(shape= JsonFormat.Shape.STRING, pattern= "MM-dd-yyyy HH:mm:ss")
     private LocalDateTime dataCriacao;
 
     @JsonFormat(shape= JsonFormat.Shape.STRING, pattern= "MM-dd-yyyy HH:mm:ss")
     private LocalDateTime dataEvento;
 
     private String emailUsuario;
+
+    @JsonFormat(shape= JsonFormat.Shape.STRING, pattern= "MM-dd-yyyy HH:mm:ss")
     private LocalDateTime dataAlteracao;
 
     private StatusNotificacao statusNotificacao;
--- src/main/java/com/grazielle_anaia/agendatarefas/business/mapper/TarefasConverter.java
+++ src/main/java/com/grazielle_anaia/agendatarefas/business/mapper/TarefasConverter.java
@@ -4,11 +4,22 @@ package com.grazielle_anaia.agendatarefas.business.mapper;
 import com.grazielle_anaia.agendatarefas.business.dto.TarefasDTO;
 import com.grazielle_anaia.agendatarefas.infrastructure.entity.TarefasEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface TarefasConverter {
 
+
+    @Mapping(source ="id", target= "id")
+    @Mapping(source = "dataEvento", target = "dataEvento")
+    @Mapping(source = "dataCriacao", target = "dataCriacao")
     TarefasEntity paraTarefasEntity(TarefasDTO dto);
 
     TarefasDTO paraTarefasDTO(TarefasEntity entity);
+
+    List<TarefasEntity> paraListaTarefasEntity(List<TarefasDTO> dto);
+
+    List<TarefasDTO> paraListaTarefasDTO(List<TarefasEntity> entity);
 }
